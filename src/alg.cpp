b92b740537a0c55ab94213403fe90dcdeae4b24d
hellbovh <69521565+hellbovh@users.noreply.github.com>
2023-02-17 15:07:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,18 +1,18 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    int res;
-    if (a < 0)
-		a *= -1;
-	if (b < 0)
-		b *= -1;
-	while (a != b)
-	{
-		if (a > b)
-			a = a - b;
-		else
-			b = b - a;
-	}
-	res = a;
-	return res;
+        int res;
+        if (a < 0)
+                a *= -1;
+        if (b < 0)
+                b *= -1;
+        while (a != b)
+        {
+                if (a > b)
+                        a - a - b;
+                else
+                        b = b - a;
+        }
+        res = a;
+        return res;
 }
